c5566cd71f84123156d930bfb235f90c2d45138a
ERAY ŞENGÜL <128752864+erysngl@users.noreply.github.com>
2026-09-16 13:43:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
-const CACHE_NAME = 'tcqr-v1';
+const CACHE_NAME = 'tcqr-v3';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
